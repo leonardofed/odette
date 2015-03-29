0c74d1a53bf17ef7e306f1c6c7627df4f748a4e2
Leonardo Federico <leonardo.fed@gmail.com>
2015-03-29 18:55:43 +0200

    Render Markers

--- application.js
+++ application.js
@@ -1,24 +1,25 @@
   L.mapbox.accessToken = 'pk.eyJ1IjoibGlxdWlkMTk4MiIsImEiOiJCdmxWQkZNIn0.itcodaqRcLopL_0WP5Rjww';
   var map = L.mapbox.map('my-map', 'examples.map-i86nkdio');
 
-  map.setView([41, 9], 6);
+  map.setView([41, 9], 7);
 
-  var marker = L.marker([41, 9]);
+  // var marker = L.marker([41, 9]);
+  // marker.addTo(map);
+  // marker.bindPopup("<strong>Ciao!</strong>");
+  // $.getJSON("data.json", function(data){
+  //   console.log('server served me this', data)
+  // });
 
-  setTimeout(function() {
-    marker.addTo(map);
-  }, 2000);
+  var callback = function(data){
+    console.log('server served me this', data["markers"]);
+    var markers = data["markers"];
+  console.log(typeof markers);
+  $(markers).each(function(index, marker){
+  console.log("Markers", marker);
+  var m = L.marker(marker['coords']);
+  m.addTo(map);
+  m.bindPopup('<b>'+ marker['title'] +'</b>');
 
-  setTimeout(function() {
-    marker.setLatLng([42, 9]);
-    marker.openPopup();
-    marker.dragging.enable();
-  }, 3000);
-
-  setTimeout(function() {
-    marker.setLatLng([93, 1]);
-    marker.openPopup();
-    marker.dragging.enable();
-  }, 7000);
-
-  marker.bindPopup("<strong>Ciao!</strong>");
+  });
+  }
+  $.getJSON('data.json', callback);
